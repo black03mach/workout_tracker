18b6f3296fd0e9a83953825ed21fddf223f0682b
Patrick Wells <pat.taylorwells@gmail.com>
2020-10-13 16:16:16 -0400

    routeFix

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pulblic/stats.html'))
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
 })
 
 router.get('/exercise', (req, res) => {
